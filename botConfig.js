c8823d8abc4fdfc5e7d25aac1edd202fde37a375
ParcifcNoob <71524914+ParcifcNoob@users.noreply.github.com>
2021-05-24 06:55:48 +0100

    Add files via upload

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "NzMwMTU0OTc4ODIzNTAzOTEy.XwTX0A.FhAIZ7ysr2j8l9gLCKoQMIcB9bg",
+    prefix: "w.",
+    admins: [
+        "690855606432694332"
+],
+    debug: true,
+    countChannel: "countChannelID"
+};
---
+++ commands/help.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.execute = async (client, message, args) => {
+    const embed = new MessageEmbed()
+        .setAuthor("Commands")
+        .setTitle("Westfield Economy")
+        .setURL("https://westfieldshoppingrblx.co.uk")
+        .setDescription(`Total Commands: ${client.commands.size}`)
+        .setColor("BLURPLE")
+        .setTimestamp()
+        .setThumbnail(client.user.displayAvatarURL)
+        .setFooter(message.author.tag, message.author.displayAvatarURL);
+    client.commands.forEach(cmd => {
+        embed.addField(`${cmd.help.name}`, `Aliases: ${cmd.help.aliases.join(", ") || "None"}\nUsage: \`${client.prefix}${cmd.help.usage}\``, true);
+    });
+    return message.channel.send(embed);
+}
+
+exports.help = {
+    name: "help",
+    aliases: ["h"],
+    usage: `help`
+}
